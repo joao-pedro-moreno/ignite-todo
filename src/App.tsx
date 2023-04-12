43ef90cdf45732a68a3cdf9c2e3556f62887a1d4
João Pedro Moreno <devmoreno2003@gmail.com>
2023-04-12 12:19:13 -0300

    Finished basic styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,38 @@
 import { Header } from './components/Header'
+import { ClipboardText, PlusCircle } from '@phosphor-icons/react'
+
+import styles from './App.module.css'
 
 import './global.css'
 
 export function App() {
 
   return (
-    <>
+    <div>
       <Header />
+      <form className={styles.form}>
+        <input type="text" placeholder='Adicione uma nova tarefa'/>
+        <button type="submit">Criar <PlusCircle size={16} weight="bold" /></button>
+      </form>
 
-      <main>
-        
+      <main className={styles.main}>
+        <header>
+          <div className={styles.created}>
+            Tarefas criadas <span>0</span>
+          </div>
+          <div className={styles.completed}>
+            Concluídas <span>0</span>
+          </div>
+        </header>
+
+        <div className={styles.tasks}>
+          <div className={styles.noTasks}>
+            <ClipboardText size={56} />
+            <strong>Você ainda não tem tarefas cadastradas</strong>
+            <p>Crie tarefas e organize seus itens a fazer</p>
+          </div>
+        </div>
       </main>
-    </>
+    </div>
   )
 }
